ddda4b5558eb3c6136c5f4372a1fb68def9d5d4c
Iftekhar881 <114158574+Iftekhar881@users.noreply.github.com>
2022-10-01 12:34:43 +0530

    Add files via upload

---
+++ Child.java
@@ -0,0 +1,17 @@
+package CoreJAva;
+
+class child extends Parent
+{
+//Creating method's of this class
+	// Method :1
+	public void speak() 
+	{
+		System.out.println("Anshu is a good speaker");
+	}
+	//Method:2
+	public void dance()
+	{
+		System.out.println("Anshu is not a good dancer");
+	}
+
+}
