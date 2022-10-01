539f4c458f8c0940b236e429a2a8d910149bd4b6
Iftekhar881 <114158574+Iftekhar881@users.noreply.github.com>
2022-10-01 12:33:57 +0530

    Add files via upload

---
+++ Versionjava.java
@@ -0,0 +1,19 @@
+package CoreJAva;
+
+/*
+program to find version of java
+@author :- Iftekhar Pathan
+Date    :- Thursday,sep 29
+*/
+
+class Versionjava
+{
+	 //Callling main method
+	 public static void main(String args[]) 
+	 {
+		 //printing version of java
+		 System.out.println("Version of the java is :"+System.getProperty("java.version"));
+	 }
+	 
+
+}
